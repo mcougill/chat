a2104ccd935669cac31b6a2bc05c91e389e6f023
Molly Cougill <molly.cougill@gmail.com>
2018-06-30 11:57:51 -0500

    start application

---
+++ src/actions/index.js
@@ -0,0 +1,22 @@
+import * as types from "../constants/ActionTypes";
+
+let nextMessageId = 0;
+const nextUserId = 0;
+
+export const addMessage = (message, author) = ({
+    type: types.ADD_MESSAGE,
+    id: nextMessageId++,
+    message,
+    author
+});
+
+export const addUser = (name) => ({
+    type: types.ADD_USER,
+    id: nextUserId++,
+    name
+});
+
+export const messageReceived = (message, author) => ({
+    type: types.MESSASGE_RECEIVED,
+    id: nextMessageId++,
+});
